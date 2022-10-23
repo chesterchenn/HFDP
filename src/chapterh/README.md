4912f8e2b6bbd03373199db20ca2db02698f15a5
chesterchenn <chesterchenn@gmail.com>
2022-10-23 21:37:19 +0800

    feat: ✨模板方法模式简单示例

---
+++ src/chapterh/README.md
@@ -0,0 +1,9 @@
+# 模板方法模式
+
+## a
+
+一个简单的煮咖啡和茶的过程。
+
+## b
+
+<div align="center"><img src="./b/uml.svg"></div>
---
+++ src/chapterh/b/Tea.java
@@ -0,0 +1,13 @@
+package chapterh.b;
+
+public class Tea extends CaffeineBeverage {
+  @Override
+  public void brew() {
+    System.out.println("Steeping the tea");
+  }
+
+  @Override
+  public void addCondiments() {
+    System.out.println("Adding Lemon");
+  }
+}
